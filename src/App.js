6d58b5db68fa91276925bf276f93b2febc5f8e00
James Pratt <jamespratt@Jamess-MacBook-Pro.local>
2021-07-31 09:38:42 -1000

    swapped some copy

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ function App() {
       <div className="school backside"><h4>📚</h4>I am a current undeclared student at Santa Clara in the Leavey School of Buisness. With a passion for
 many fields from Information Systems to Real 
 Estate. </div>
-<div className="work backside"><h4>💼</h4>Previously, I worked at Hokuala, the local golf course, as a Cart Boy. I volunteered at Nalukai Academy as a team lead. </div>
+<div className="work backside"><h4>💼</h4>Previously, I worked at Hokuala, the local golf course, as a Cart Boy. Also, I volunteered at Nalukai Academy as a team lead. </div>
 
 <div className="circles"><div className="circ1"></div>
 <div className="circ2"></div>
 <div className="circ3"></div></div>
-<div className="now backside"><h4>📅</h4>Currently, I am learning more about the world of Commercial Real Estate by working at the family buisness. </div>
+<div className="now backside"><h4>📅</h4>Currently, I am learning more about the world of Commercial Real Estate by working at the family buisness, taking online classes and working at the local country club. </div>
 
 <div className="skills backside"><h4>💻</h4>I am profecient with Excel. Beyond that I frequently explore my passion in programming with web development tools like react and ocassionally python.</div>
 
