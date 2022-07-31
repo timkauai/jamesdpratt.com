92270ae9dd1e7998ade68fd6cffb07c2d5ec44f1
James Pratt <jamespratt@Jamess-MacBook-Pro.local>
2022-07-31 13:32:06 -1000

    recent revamp

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="header"><h3>👋  Hey!</h3><h1>I'm James D. Pratt</h1><div className="portrait"></div></div>
-      
-      <div className="school backside"><h4>📚</h4>I am a current finance student at Santa Clara in the Leavey School of Buisness, with a minor in Real Estate. </div>
-<div className="work backside"><h4>💼</h4>Previously, I worked at Hokuala, the local golf course, as a Cart Boy. Also, I volunteered at Nalukai Academy as a team lead. </div>
-
-<div className="circles"><div className="circ1"></div>
-<div className="circ2"></div>
-<div className="circ3"></div></div>
-<div className="now backside"><h4>📅</h4>Currently, I am learning more about the world of Commercial Real Estate by working at the family buisness, taking online classes and working at the local country club. </div>
-
-<div className="skills backside"><h4>💻</h4>I am profecient with Excel. Beyond that I frequently explore my passion in programming with web development tools like react and ocassionally python.</div>
+      <div className="grid">
+      <div className="header"><h3>👋  Hey!</h3><h1>I'm James D. Pratt</h1></div>
+      <div className="portrait"></div>
+      <div className="copy">
+        <div className="school backside"><h4>📚</h4>I am a current finance student at Santa Clara in the Leavey School of Buisness, with a minor in Real Estate, and a minor in Data Analytics. </div>
+        <div className="work backside"><h4>💼</h4>Over the summer of 2022, I worked at the County of Kauai in the Real Property assessment division. I learned how people in the industry value properties, specifically using cost and market methods of valuation. </div>
+        <div className="now backside"><h4>💻</h4>Through classes, projects and work experiences, I have proved my proficency in web development (with HTML, CSS and react.js) and python as well as in Microsoft office applications.</div>
+      </div>
+      <div className="proj"></div>
+    </div>
 
     </div>
   );
