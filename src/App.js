47072b03ea9e2f9be13a1b50046b3da5b4ff8afc
James Pratt <jamespratt@Jamess-MacBook-Pro.local>
2022-07-31 13:39:01 -1000

    redesign

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="copy">
         <div className="school backside"><h4>📚</h4>I am a current finance student at Santa Clara in the Leavey School of Buisness, with a minor in Real Estate, and a minor in Data Analytics. </div>
         <div className="work backside"><h4>💼</h4>Over the summer of 2022, I worked at the County of Kauai in the Real Property assessment division. I learned how people in the industry value properties, specifically using cost and market methods of valuation. </div>
-        <div className="now backside"><h4>💻</h4>Through classes, projects and work experiences, I have proved my proficency in web development (with HTML, CSS and react.js) and python as well as in Microsoft office applications.</div>
+        <div className="now backside"><h4>💻</h4>Through classes, projects and work experiences, I have proved my proficency in web development (with HTML, CSS and react.js) and python.</div>
       </div>
       <div className="proj"></div>
     </div>
